d5faeaa5560d1388e8c48dff2233ca705a377eb8
Paolo Perego <thesp0nge@gmail.com>
2013-10-11 12:01:48 +0200

    Changed default syslog sending not to debug

--- lib/codesake/commons/logging.rb
+++ lib/codesake/commons/logging.rb
@@ -49,8 +49,8 @@ module Codesake
       def log(msg)
         return if @silencer
         STDOUT.print "#{Time.now.strftime("%H:%M:%S")}: #{msg}\n".color(:white)
-        send_to_syslog(msg, :debug)
-        send_to_file(msg, :debug)
+        send_to_syslog(msg, :log)
+        send_to_file(msg, :log)
       end
 
       def helo(msg, pid_file = nil)
@@ -85,11 +85,11 @@ module Codesake
 
         log = Syslog.open("codesake") unless Syslog.opened?
         log = Syslog.reopen("codesake") if Syslog.opened?
-        Syslog.log(Syslog::LOG_DEBUG, msg.to_s)  if level == :debug
-        Syslog.log(Syslog::LOG_WARNING, msg.to_s)   if level == :warn
-        Syslog.log(Syslog::LOG_INFO, msg.to_s)   if level == :helo
-        Syslog.log(Syslog::LOG_INFO, msg.to_s) if level == :log
-        Syslog.log(Syslog::LOG_ERR, msg.to_s)    if level == :error 
+        log.log(Syslog::LOG_DEBUG, msg.to_s)  if level == :debug
+        log.log(Syslog::LOG_WARNING, msg.to_s)   if level == :warn
+        log.log(Syslog::LOG_INFO, msg.to_s)   if level == :helo
+        log.log(Syslog::LOG_INFO, msg.to_s) if level == :log
+        log.log(Syslog::LOG_ERR, msg.to_s)    if level == :error 
           
         true
       end
